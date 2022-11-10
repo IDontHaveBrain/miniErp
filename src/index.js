b1cbb3386c09b501cf654f2ad9dadaebcf082748
NamJun Jo <jonamjun.dev@gmail.com>
2022-11-10 20:47:27 +0900

    기본설정2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'mobx-react';
+import RootStore from "./store/RootStore";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider rootStore={...RootStore}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
