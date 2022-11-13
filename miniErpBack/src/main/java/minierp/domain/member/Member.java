4cf08a43c036d593ada484013c6918ac2c6a7101
chamatung <ckguswls312@gmail.com>
2022-11-13 21:16:04 +0900

    miniErpBack

---
+++ miniErpBack/src/main/java/minierp/domain/member/Member.java
@@ -0,0 +1,27 @@
+package minierp.domain.member;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long memberId; //사번
+
+    @NotNull
+    private String pw;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Role role; //권한
+}
---
+++ miniErpBack/src/main/java/minierp/web/ErpController.java
@@ -0,0 +1,11 @@
+package minierp.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class ErpController {
+
+}
