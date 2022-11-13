8334441431b8a8f14de26b2d582e1e9e1da0caf8
NamJun Jo <jonamjun.dev@gmail.com>
2022-11-13 21:29:31 +0900

    프론트, 백엔드 분리

--- front/src/component/Deposits.js
+++ front/src/component/Deposits.js
@@ -18,7 +18,7 @@ export default function Deposits() {
         on 15 March, 2019
       </Typography>
       <div>
-        <Link color="primary" href="#" onClick={preventDefault}>
+        <Link color="primary" href="front/src/component/Deposits#" onClick={preventDefault}>
           View balance
         </Link>
       </div>
--- front/src/component/Orders.js
+++ front/src/component/Orders.js
@@ -78,7 +78,7 @@ export default function Orders() {
           ))}
         </TableBody>
       </Table>
-      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+      <Link color="primary" href="front/src/component/Orders#" onClick={preventDefault} sx={{ mt: 3 }}>
         See more orders
       </Link>
     </React.Fragment>
